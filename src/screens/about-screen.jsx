ee4df27f13cad684459ccc91b418b267c68b296c
SUMAN KUMAR <86798449+imSuMN@users.noreply.github.com>
2023-05-28 23:47:37 +0530

    avatar added to the aboutScreen

--- src/screens/about-screen.jsx
+++ src/screens/about-screen.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { Flex } from "native-base";
+import { Avatar } from "native-base";
 
 const AboutScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Avatar source={{ uri: "https://placebear.com/400/400" }} size="xl" />
       <Text>AboutScreen</Text>
     </View>
   );
