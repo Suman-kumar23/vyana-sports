d776df5e785ea3d264e9414243e62203bcaed20d
SUMAN KUMAR <86798449+imSuMN@users.noreply.github.com>
2023-05-28 23:37:06 +0530

    Changed the bottom-tab icons

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
-import MaterialCommunityIcons from "react-native-vector-icons";
 
 // Screens
 import MainScreen from "./screens/main-screen";
@@ -9,85 +8,45 @@ import AboutScreen from "./screens/about-screen";
 import LiveScreen from "./screens/live-screen";
 import PhotoScreen from "./screens/photo-screen";
 
-// Components
-// import Sidebar from "./components/sidebar";
-
 const Tab = createBottomTabNavigator();
 
 const App = () => {
-  const screenOptions = {
-    tabBarOptions: {
-      fontSize: 40,
-    },
-    tabBarStyle: {
-      height: 75,
-    },
-    tabBarItemStyle: {
-      marginBottom: 15,
-      // fontSize: 40,
-    },
-    headerShown: false,
-    tabBarActiveTintColor: "#e91e63",
-  };
-
   return (
-    <Tab.Navigator initialRouteName="Home" {...{ screenOptions }}>
-      <Tab.Screen
-        name="Home"
-        component={MainScreen}
-        options={{
-          tabBarLabel: "Home",
-          tabBarIcon: (tabInfo) => {
-            <Ionicons
-              name="md-home"
-              size={tabInfo.focused ? 26 : 20}
-              color={tabInfo.tintColor}
-            />;
-          },
-        }}
-      />
-      <Tab.Screen
-        name="Live"
-        component={LiveScreen}
-        options={{
-          tabBarLabel: "Live",
-          tabBarIcon: (tabInfo) => {
-            <Ionicons
-              name="md-home"
-              size={tabInfo.focused ? 26 : 20}
-              color={tabInfo.tintColor}
-            />;
-          },
-        }}
-      />
-      <Tab.Screen
-        name="Photos"
-        component={PhotoScreen}
-        options={{
-          tabBarLabel: "Photos",
-          tabBarIcon: (tabInfo) => {
-            <Ionicons
-              name="md-home"
-              size={tabInfo.focused ? 26 : 20}
-              color={tabInfo.tintColor}
-            />;
-          },
-        }}
-      />
-      <Tab.Screen
-        name="About"
-        component={AboutScreen}
-        options={{
-          tabBarLabel: "About",
-          tabBarIcon: (tabInfo) => {
-            <Ionicons
-              name="md-home"
-              size={tabInfo.focused ? 26 : 20}
-              color={tabInfo.tintColor}
-            />;
-          },
-        }}
-      />
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, size, color }) => {
+          let iconName;
+          if (route.name === "Home") {
+            iconName = focused ? "ios-home" : "ios-home-outline";
+            size = focused ? size + 8 : size + 5;
+          } else if (route.name === "Live") {
+            iconName = focused ? "ios-tv" : "ios-tv-outline";
+            size = focused ? size + 8 : size + 5;
+          } else if (route.name === "Photos") {
+            iconName = focused ? "image" : "image-outline";
+            size = focused ? size + 8 : size + 5;
+          } else if (route.name === "About") {
+            iconName = focused ? "person-sharp" : "person-outline";
+            size = focused ? size + 8 : size + 5;
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        headerShown: false,
+        tabBarActiveTintColor: "#e91e63",
+      })}
+      tabBarOptions={{
+        showLabel: false,
+        tabBarActiveTintColor: "#e91e63",
+        style: {
+          height: 75,
+        },
+      }}
+    >
+      <Tab.Screen name="Home" component={MainScreen} />
+      <Tab.Screen name="Live" component={LiveScreen} />
+      <Tab.Screen name="Photos" component={PhotoScreen} />
+      <Tab.Screen name="About" component={AboutScreen} />
     </Tab.Navigator>
   );
 };
